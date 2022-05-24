a1543788a420457e20377fffb076943e3b801036
Alex Liebman-Pelaez <alexliebmanp@gmail.com>
2022-05-24 12:17:02 -0700

    wrote a real first draft of README and cleaned up basic_test.py, as well as adding an image of the correct output.

--- tests/basic_test.py
+++ tests/basic_test.py
@@ -1,4 +1,3 @@
-# %% codecell
 import os
 from orenstein_analysis.measurement import loader
 from orenstein_analysis.measurement import process
@@ -8,10 +7,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# %% codecell
-#path = os.path.dirname(os.path.realpath(__file__))+'/test_data/'
-path = '/Users/oxide/orenstein/code/orenstein-analysis/tests/test_data/'
-filename = 'EuIn2As2_S2_BR_9K_x2740_y2540.dat'
+path = os.path.dirname(os.path.realpath(__file__))+'/test_data/'
 
 set_coord = lambda meas: process.define_dimensional_coordinates(meas, {'Polarization Angle (deg)':2*meas['Angle 1 (deg)']})
 
@@ -20,14 +16,18 @@ fit_bf = lambda meas: process.add_processed(meas, (experiment_methods.fit_birefr
 ndim_meas = loader.load_ndim_measurement(path, {'x ($\mu$m)':'_x[0-9]+', 'y ($\mu$m)':'_y[0-9]+'}, instruction_set=[set_coord, fit_bf])
 print(ndim_meas)
 
-# %% codecell
-ndim_meas['Demod x (fit)'].sel({'x ($\mu$m)':2800, 'y ($\mu$m)':2475}, method='nearest').plot(label='fit')
-ndim_meas['Demod x'].sel({'x ($\mu$m)':2800, 'y ($\mu$m)':2475}, method='nearest').plot(marker='o', ms=8, linestyle='None', label='data')
-plt.legend()
+fig, [ax1, ax2, ax3]  = plt.subplots(1,3)
 
-# %% codecell
-ndim_meas['Birefringence Angle'].plot(cmap='twilight')
-plt.title(r'EuIn$_2$As$_2$')
+ndim_meas['Demod x (fit)'].sel({'x ($\mu$m)':2800, 'y ($\mu$m)':2475}, method='nearest').plot(ax=ax1, label='fit')
+ndim_meas['Demod x'].sel({'x ($\mu$m)':2800, 'y ($\mu$m)':2475}, method='nearest').plot(ax=ax1, marker='o', ms=8, linestyle='None', label='data')
+ax1.legend()
+ax1.set_title('Birefringence fit at x = 2800 and y = 2475')
 
-# %% codecell
-xr.plot.hist(ndim_meas['Birefringence Angle'])
+ndim_meas['Birefringence Angle'].plot(ax=ax2, cmap='twilight')
+ax2.set_title(r'EuIn$_2$As$_2$')
+
+xr.plot.hist(ndim_meas['Birefringence Angle'], ax=ax3)
+
+fig.set_size_inches(12, 4, forward=True)
+plt.tight_layout()
+plt.show()
